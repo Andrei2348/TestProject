694ff492c62e6a8d14fd95c6e09b323939725fb4
Andrei2348 <makarevichandco@gmail.com>
2024-02-10 11:42:38 +0300

    Доработки

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,37 @@
-let inputName = document.getElementById('input__text');
-let inputEmail = document.getElementById('input__email');
-let inputPhone = document.getElementById('input__phone');
-let inputMessage = document.getElementById('input__message');
+// let inputName = document.getElementById('input__text');
+// let inputEmail = document.getElementById('input__email');
+// let inputPhone = document.getElementById('input__phone');
+// let inputMessage = document.getElementById('input__message');
+const formInputs = document.querySelectorAll('.message__form-inner .form-input');
 import {validate} from './modules/validate.js'
 
+
 window.addEventListener('DOMContentLoaded', startProp);
+
+
+// Функция получения данных из формы
+function getValuesOfForm(){
+    let data = []
+    for (let i = 0; i < 4; i++){
+        data.push(formInputs[i].value)
+    }
+    return data;
+}
 
 function startProp(){
     document.querySelector('.message__form-button').addEventListener('click', function(event){
         event.preventDefault();
+        let formData = getValuesOfForm();
         
-        let formData = {
-            name: inputName.value,
-            email: inputEmail.value,
-            phone: inputPhone.value,
-            message: inputMessage.value
-        };
-        console.log(formData)
+
+
+        // let formData = {
+        //     name: inputName.value,
+        //     email: inputEmail.value,
+        //     phone: inputPhone.value,
+        //     message: inputMessage.value
+        // };
+        // console.log(formData)
         
         if(validate(formData)){
             console.log('Делаем запрос на сервер')
--- scripts/modules/validate.js
+++ scripts/modules/validate.js
@@ -1,41 +1,36 @@
 let reArray = [/^[a-zA-Z0-9]+$/, 
                 /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i, 
-                /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
+                /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/,
+                /^(\d{4})$/
             ]
 
 let messagesArray = ['Имя пользователя введено неправильно!',
                     'Адрес электронной почты введен неправильно!',
-                    'Номер телефона введен неправильно!'
+                    'Номер телефона введен неправильно!',
+                    ''
                 ]
 
 
 
 export function validate(data){
-    console.log(data)
-    for (let key of Object.keys(data)) {
-
-        console.log(data[key])
-        console.log(data.findIndex(data[key]))
-        // let index = data.findIndex(p => p.attr1 == "john");
-    }
     let errors = [];
+    console.log(data)
+    for (let i = 0; i < 4; i++){
+        errors.push(checkValid(i, data[i]))
+    }
     
-
     
-    errors.push(validName(data.name))
-    errors.push(validMail(data.email))
-    errors.push(validPhone(data.phone))
-    return errors
+    return arr.some(errors)
 }
 
 
-function validName(login){
+function checkValid(index, data){
     let output = '';
     let valid = false;
-    if(login.length > 0){
-        const re = /^[a-zA-Z0-9]+$/;
-        valid = re.test(login);
-        if(!valid){output = 'Имя пользователя введено неправильно!'};
+    if(data.length > 0){
+        const re = reArray[index];
+        valid = re.test(data[index]);
+        if(!valid){output = messagesArray[index]};
     } else {
         output = 'Это поле не может быть пустым!'
     }
@@ -44,35 +39,35 @@ function validName(login){
 };
 
 
-function validMail(email) {
-    let output = '';
-    let valid = false
-    if(email.length > 0){
-        const re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-        valid = re.test(email);
-        if(!valid){output = 'Адрес электронной почты введен неправильно!'};
-    } else {
-        output = 'Это поле не может быть пустым!'
-    }
-    document.getElementById('error__email').innerHTML = output;
-    return valid;
-}
+// function validMail(email) {
+//     let output = '';
+//     let valid = false
+//     if(email.length > 0){
+//         const re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+//         valid = re.test(email);
+//         if(!valid){output = 'Адрес электронной почты введен неправильно!'};
+//     } else {
+//         output = 'Это поле не может быть пустым!'
+//     }
+//     document.getElementById('error__email').innerHTML = output;
+//     return valid;
+// }
 
 
-function validPhone(phone) {
-    let output = '';
-    let valid = false;
-    if(phone.length > 0){
-        var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-        valid = re.test(phone);
-        console.log(valid)
-        if(!valid){output = 'Номер телефона введен неправильно!'};
-    } else {
-        output = 'Это поле не может быть пустым!'
-    }
-    document.getElementById('error__phone').innerHTML = output;
-    return valid;
-} 
+// function validPhone(phone) {
+//     let output = '';
+//     let valid = false;
+//     if(phone.length > 0){
+//         var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+//         valid = re.test(phone);
+//         console.log(valid)
+//         if(!valid){output = 'Номер телефона введен неправильно!'};
+//     } else {
+//         output = 'Это поле не может быть пустым!'
+//     }
+//     document.getElementById('error__phone').innerHTML = output;
+//     return valid;
+// } 
 
 
 
